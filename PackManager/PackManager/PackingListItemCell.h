582ff46f51d1b72731a4b16331e455070043bcf3
hblockhus <hblockhus@ucla.edu>
2015-04-29 18:42:56 -0700

    another small change

--- PackManager/PackManager/PackingListItemCell.h
+++ PackManager/PackManager/PackingListItemCell.h
@@ -7,6 +7,7 @@
 //
 
 #import <UIKit/UIKit.h>
+#import "Packable.h"
 
 @interface PackingListItemCell : UITableViewCell
 
--- PackManager/PackManager/PackingListItemCell.m
+++ PackManager/PackManager/PackingListItemCell.m
@@ -7,7 +7,6 @@
 //
 
 #import "PackingListItemCell.h"
-#import "Packable.h"
 
 @implementation PackingListItemCell
 
