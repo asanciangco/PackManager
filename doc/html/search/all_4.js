90cf993d5e5606a583f2c2903d8e3775f4547433
Steven Holtzen <sholtzen@gmail.com>
2015-05-03 16:46:05 -0700

    Added Doxyfile for automated documentation generation, can be run with 'doxygen Doxyfile'. Created docs/ directory with documentation.

---
+++ doc/html/search/all_4.js
@@ -0,0 +1,13 @@
+var searchData=
+[
+  ['pack_20manager',['Pack Manager',['../md__r_e_a_d_m_e.html',1,'']]],
+  ['packable',['Packable',['../interface_packable.html',1,'']]],
+  ['packinglist',['PackingList',['../interface_packing_list.html',1,'']]],
+  ['packinglist_28_29',['PackingList()',['../category_packing_list_07_08.html',1,'']]],
+  ['packinglistitemcell',['PackingListItemCell',['../class_packing_list_item_cell.html',1,'']]],
+  ['packinglistviewcontroller',['PackingListViewController',['../interface_packing_list_view_controller.html',1,'']]],
+  ['packinglistviewcontroller_28_29',['PackingListViewController()',['../category_packing_list_view_controller_07_08.html',1,'']]],
+  ['packmanagertests',['PackManagerTests',['../interface_pack_manager_tests.html',1,'']]],
+  ['profileviewcontroller',['ProfileViewController',['../interface_profile_view_controller.html',1,'']]],
+  ['profileviewcontroller_28_29',['ProfileViewController()',['../category_profile_view_controller_07_08.html',1,'']]]
+];
---
+++ doc/html/search/classes_4.js
@@ -0,0 +1,12 @@
+var searchData=
+[
+  ['packable',['Packable',['../interface_packable.html',1,'']]],
+  ['packinglist',['PackingList',['../interface_packing_list.html',1,'']]],
+  ['packinglist_28_29',['PackingList()',['../category_packing_list_07_08.html',1,'']]],
+  ['packinglistitemcell',['PackingListItemCell',['../class_packing_list_item_cell.html',1,'']]],
+  ['packinglistviewcontroller',['PackingListViewController',['../interface_packing_list_view_controller.html',1,'']]],
+  ['packinglistviewcontroller_28_29',['PackingListViewController()',['../category_packing_list_view_controller_07_08.html',1,'']]],
+  ['packmanagertests',['PackManagerTests',['../interface_pack_manager_tests.html',1,'']]],
+  ['profileviewcontroller',['ProfileViewController',['../interface_profile_view_controller.html',1,'']]],
+  ['profileviewcontroller_28_29',['ProfileViewController()',['../category_profile_view_controller_07_08.html',1,'']]]
+];
