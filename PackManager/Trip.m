2e58ad5fa7a31f96effe3c0e3d89b50891369190
Alex Sanciangco <a.sanciangco@gmail.com>
2015-05-03 16:26:45 -0700

    finished user preference implementation within Trip class

--- PackManager/Trip.m
+++ PackManager/Trip.m
@@ -7,6 +7,7 @@
 //
 
 #import "Trip.h"
+#import "UserPreferences.h"
 
 @implementation Trip
 
@@ -19,10 +20,17 @@
 {
     if (self = [Trip init])
     {
+        UserPreferences *prefs = [UserPreferences sharedInstance];
+        
         self.startDate      = start;
         self.duration       = numDays;
         self.destination    = dest;
         self.name           = name;
+        
+        self.swimmingPreference = [prefs getSwimmingPreference];
+        self.formalPreference   = [prefs getFormalPreference];
+        self.rewearJeansPreference  = [prefs getRewearJeansPreference];
+        self.accessToLaundryPreference  = [prefs getAccessToLaundryPreference];
     }
     return self;
 }
@@ -66,6 +74,11 @@
     [aCoder encodeObject:self.startDate forKey:@"startDate"];
     [aCoder encodeInteger:self.duration forKey:@"duration"];
     [aCoder encodeObject:self.packingList forKey:@"packingList"];
+    
+    [aCoder encodeBool:self.swimmingPreference forKey:@"swimmingPreference"];
+    [aCoder encodeBool:self.formalPreference forKey:@"formalPreference"];
+    [aCoder encodeBool:self.rewearJeansPreference forKey:@"rewearJeansPreference"];
+    [aCoder encodeBool:self.accessToLaundryPreference forKey:@"accessToLaundryPreference"];
 }
 
 - (id) initWithCoder:(NSCoder *)aDecoder
@@ -77,6 +90,11 @@
         self.startDate = [aDecoder decodeObjectForKey:@"startDate"];
         self.duration = [aDecoder decodeIntegerForKey:@"duration"];
         self.packingList = [aDecoder decodeObjectForKey:@"packingList"];
+        
+        self.swimmingPreference = [aDecoder decodeBoolForKey:@"swimmingPreference"];
+        self.formalPreference = [aDecoder decodeBoolForKey:@"formalPreference"];
+        self.rewearJeansPreference = [aDecoder decodeBoolForKey:@"rewearJeansPreference"];
+        self.accessToLaundryPreference = [aDecoder decodeBoolForKey:@"accessToLaundryPreference"];
     }
     return self;
 }
