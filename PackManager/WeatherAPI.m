8db25dd6e60d44a525415e50578291e75478a42b
kaceyjryan <kaceyjryan@gmail.com>
2015-05-07 13:49:30 -0700

    Added some more logic and dicitionary parsing for the json object collected form the API

--- PackManager/WeatherAPI.m
+++ PackManager/WeatherAPI.m
@@ -41,9 +41,9 @@ static NSString *CityWeatherURLLocation = @"http://api.openweathermap.org/data/2
     
     if ((instance = [[WeatherAPI alloc] init]))
     {
-        [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(handleWeatherDictionaryZip:) name:ZIP_JSON_DATA_RETURNED_NOTIFICATION object:self];
+        //[[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(handleWeatherDictionaryZip:) name:ZIP_JSON_DATA_RETURNED_NOTIFICATION object:self];
         
-        [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(handleWeatherDictionaryZip:) name:CITY_JSON_DATA_RETURNED_NOTIFICATION object:self];
+        [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(handleWeatherDictionaryCity:) name:CITY_JSON_DATA_RETURNED_NOTIFICATION object:self];
     }
     return instance;
 }
@@ -54,8 +54,10 @@ static NSString *CityWeatherURLLocation = @"http://api.openweathermap.org/data/2
 }
 
 #pragma mark - Core Fuctions
-
+/*
+//To be used later when we implement the NOAA API (CURRENTLY NOT COMPLETE)
 - (void) getWeatherFromZip:(NSInteger)zip start:(NSDate *)start end:(NSDate *)end{
+    // TODO: Need to format the dates as strings
     NSString *WeatherUrl = [NSString stringWithFormat:@"%@datasetid=GHCND&locationid=ZIP:%@&startdate=%@&enddate=%@&sortfield", ZipWeatherURLData, zip, start, end];
     
     NSURLSessionConfiguration *sessionConfiguration = [NSURLSessionConfiguration defaultSessionConfiguration];
@@ -74,7 +76,9 @@ static NSString *CityWeatherURLLocation = @"http://api.openweathermap.org/data/2
                     options:0
                     error:&error];
                 
-                if(error) { /* JSON was malformed, act appropriately here */ }
+                if(error) { 
+                    // JSON was malformed, act appropriately here
+                }
                 
                 [[NSNotificationCenter defaultCenter] postNotificationName:ZIP_JSON_DATA_RETURNED_NOTIFICATION object:self userInfo:ZipWeatherFeatures];
                 
@@ -95,7 +99,7 @@ static NSString *CityWeatherURLLocation = @"http://api.openweathermap.org/data/2
         [tempdict enumerateKeysAndObjectsUsingBlock:^(id key, id obj, BOOL *stop) {
             if ([key  isEqual: @"date"]) {
                 //Set Date
-                //*day = ;
+                // *day = ;
             }
             if ([key  isEqual: @"datatype"]) {
                 if ([obj  isEqual: @"PRCP"]) {
@@ -116,8 +120,10 @@ static NSString *CityWeatherURLLocation = @"http://api.openweathermap.org/data/2
         
     }
 }
+ */
 
-//Still need to figure out how to parse json to only collect the number of days needed, also need to ensure the date of trip is captured by the forecast (0> end_of_trip > 16)
+
+//Gets weather for a city country combination for the next 16 days and uses that to get the weather for the upcoming trip. Country needs to be the two char country code. Takes the data from json and places it into a new dictionary that it passes to a handler to work with. Have not tested yet!
 - (void) getWeatherFromCity:(NSString*)city country:(NSString*)country start:(NSDate *)start end:(NSDate *)end
 {
     NSString *reformattedCity = [ city stringByReplacingOccurrencesOfString:@" " withString:@"+"];
@@ -129,13 +135,13 @@ static NSString *CityWeatherURLLocation = @"http://api.openweathermap.org/data/2
                                                    };
     NSURLSession *session = [NSURLSession sessionWithConfiguration:sessionConfiguration];
     
-    NSError *error = nil;
+    //NSError *error = nil;
     [[session dataTaskWithURL:[NSURL URLWithString:WeatherUrl]
             completionHandler:^(NSData *data,
                                 NSURLResponse *response,
                                 NSError *error) {
                 // handle response
-                NSDictionary *cityWeatherFeatures = [NSJSONSerialization JSONObjectWithData:response
+                NSDictionary *cityWeatherFeatures = [NSJSONSerialization JSONObjectWithData: data
                     options:0
                     error:&error];
                 
